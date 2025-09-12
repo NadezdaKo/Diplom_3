eda61b7ca7be5a2fbb37312fbbe84faea33ea8b4
Nadezhda <nadezhda.comiss@yandex.ru>
2025-09-12 09:39:27 +0500

    Файл с данными пользователя

---
+++ data.py
@@ -0,0 +1,9 @@
+class Credentials:
+    email = 'test_user40@example.com'
+    password = 'Test123456'
+
+class Data:
+    ingredient_name = '61c0c5a71d1f82001bdaaa6d'
+    order_number = '12345'
+
+global_timeout = 10
